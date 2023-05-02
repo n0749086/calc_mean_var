da00b3238b7ab1ea1425aa12fb6f583e1aaad977
isiz <n0749086@gmail.com>
2023-05-02 11:42:18 +0900

    Update incremental_statistics.py
    - add docstring

--- incremental_statistics.py
+++ incremental_statistics.py
@@ -2,7 +2,12 @@
 https://zenn.dev/utcarnivaldayo/articles/ffeed5ac2e62bb
 '''
 
-class IncrementalStatistics(object):
+class IncrementalStatistics():
+    """_summary_
+
+    Args:
+        object (_type_): _description_
+    """
     def __init__(self) -> None:
         self.__mean = 0.0
         self.__count = 0
@@ -11,13 +16,24 @@ class IncrementalStatistics(object):
         self.__c_m_2 = 0.0
 
     def clear(self) -> None:
+        """clear the variables
+        """
         self.__init__()
 
     @staticmethod
-    def kahan(sum: float, c: float, data: float):
-        '''
+    def kahan(sum: float, c: float, data: float) -> tuple:
+        """calculate with kahan method
         https://ja.wikipedia.org/wiki/%E3%82%AB%E3%83%8F%E3%83%B3%E3%81%AE%E5%8A%A0%E7%AE%97%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0
-        '''
+        
+
+        Args:
+            sum (float): sum
+            c (float): compensation variable
+            data (float): input
+
+        Returns:
+            tuple: calclated sum and c
+        """
         y = data - c
         t = sum + y
         c = (t - sum) - y
@@ -25,7 +41,12 @@ class IncrementalStatistics(object):
 
         return (sum, c)
 
-    def add_data(self, data: float) -> tuple:
+    def add_data(self, data: float):
+        """add input data
+
+        Args:
+            data (float): input
+        """
         self.__count += 1
         # meanの計算
         delta = data - self.__mean
@@ -35,16 +56,31 @@ class IncrementalStatistics(object):
         delta_var = data - self.__mean
         self.__m_2, self.__c_m_2 = self.kahan(self.__m_2, self.__c_m_2, delta * delta_var)
 
-    def get_mean(self):
+    def get_mean(self) -> float:
+        """return mean value
+
+        Returns:
+            float: mean value
+        """
         return self.__mean
-    
-    def get_variance(self):
+
+    def get_variance(self) -> float:
+        """return variance
+
+        Returns:
+            float: variance
+        """
         if self.__count == 0:
             return 0.0
         else:
             return self.__m_2 / self.__count
-    
+
     def get_unvariance(self) -> float:
+        """return unbiased variance
+
+        Returns:
+            float: unbiased variance
+        """
         if self.__count == 0:
             return 0.0
         else:
